c5543652758b8e11adc322bb8b6d99e79a0a23ab
jsjung00 <justinsoljung@gmail.com>
2022-08-22 11:59:20 -0500

    update projects and add connect four post

--- data/projectsData.js
+++ data/projectsData.js
@@ -6,6 +6,18 @@ const projectsData = [
     href: 'https://github.com/jsjung00/eat-candy/',
   },
   {
+    title: 'Connect 4 AI',
+    description: `Developed competitive Connect 4 computer player using Minimax algorithm, alpha beta pruning with heuristic search`,
+    imgSrc: '/static/images/connect_four_logo.png',
+    href: 'https://jsjung00.github.io/connect-four/',
+  },
+  {
+    title: 'Martingale Betting Simulator',
+    description: `Simulates the martingale betting strategy using a fixed starting capital amount`,
+    imgSrc: '/static/images/martingale_simulator.png',
+    href: 'https://jsjung00.github.io/martingale-betting-simulator/',
+  },
+  {
     title: 'Building Shooter Game as Fast as Possible',
     description: `I wanted to see how much of a 2D shooter game like surviv.io I could build in one sitting; surprised by how much can be done in a few hours`,
     imgSrc: '/static/images/shooter.png',
