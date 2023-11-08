1e9c84613127961e00aabac2ed3274ac675c4106
jsjung00 <justinsoljung@gmail.com>
2023-11-07 20:47:35 -0800

    update files

--- data/siteMetadata.js
+++ data/siteMetadata.js
@@ -9,7 +9,7 @@ const siteMetadata = {
   siteRepo: 'https://github.com/jsjung00/jsjung00.github.io',
   siteLogo: '/static/images/logo.png',
   image: '/static/images/avatar.png',
-  socialBanner: '/static/images/twitter-card.png',
+  socialBanner: '/static/images/deep_exploration_logo.png',
   email: 'justinsoljung@gmail.com',
   github: 'https://github.com/jsjung00',
   //twitter: 'https://twitter.com/Twitter',
