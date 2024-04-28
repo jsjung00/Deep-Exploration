b8e5d0407fe66b56d21bbfefa353daec0eb799d2
Justin Jung <justinsoljung@gmail.com>
2024-04-27 20:44:17 -0700

    add peresonal section; all personal blogs start with personal in file name git status

--- data/headerNavLinks.js
+++ data/headerNavLinks.js
@@ -2,6 +2,7 @@ const headerNavLinks = [
   { href: '/blog', title: 'Blog' },
   { href: '/projects', title: 'Projects' },
   { href: '/tags', title: 'Publications' },
+  { href: '/personal', title: 'Personal' },
   { href: '/about', title: 'About' },
 ]
 
