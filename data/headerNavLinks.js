f35e9abcd8ebf875b88c064aca570e558bd61fb7
Justin Jung <justinsoljung@gmail.com>
2024-07-11 08:00:51 -0700

    remove projects section

--- data/headerNavLinks.js
+++ data/headerNavLinks.js
@@ -1,6 +1,6 @@
 const headerNavLinks = [
   { href: '/blog', title: 'Blog' },
-  { href: '/projects', title: 'Projects' },
+  //{ href: '/projects', title: 'Projects' },
   //{ href: '/personal', title: 'Personal' },
   { href: '/about', title: 'About' },
 ]
